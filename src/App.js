e47e3ec3cff2734ce6f26d342f091e5e015606de
thosijulio <thosijulio@gmail.com>
2022-06-24 10:06:06 -0300

    feat - Criacao da pagina Not foind e configuracao da rota dessa pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Pokemon from './pages/Pokemon';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/pokemon" element={<Pokemon />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
