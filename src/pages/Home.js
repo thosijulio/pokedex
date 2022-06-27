4b94932fd8f819dd4425401f8d0aeca65fe09f22
thosijulio <thosijulio@gmail.com>
2022-06-27 11:22:34 -0300

    feat - finalização tela Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,9 +9,24 @@ function Home() {
           fan. if you have any questions, please get in touch.
         </h3>
         <div className="linked-icons">
-          <a className="fa-brands fa-github" href="https://www.github.com/thosijulio" />
-          <a className="fa-brands fa-linkedin" />
-          <a className="fa-brands fa-instagram" />
+          <a
+            className="fa-brands fa-github"
+            href="https://www.github.com/thosijulio"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <a
+            className="fa-brands fa-linkedin"
+            href="https://linkedin.com/in/thosijulio"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <a
+            className="fa-brands fa-instagram"
+            href="https://instagram.com/thosijulio"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
         </div>
       </section>
     </main>
