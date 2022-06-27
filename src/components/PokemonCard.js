dda44b47d747b49ff9f9e24a4108cb10b19ec4f4
thosijulio <thosijulio@gmail.com>
2022-06-27 16:15:14 -0300

    style - posicionando header do card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -3,6 +3,10 @@ import getPokemonByName from '../services/getPokemonByName';
 import PropTypes from 'prop-types';
 import './PokemonCard.css';
 
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.substring(1);
+};
+
 function PokemonCard({ name }) {
   const [pokemon, setPokemon] = useState({});
 
@@ -14,10 +18,15 @@ function PokemonCard({ name }) {
 
   if (Object.keys(pokemon).length) {
     return (
-      <div className="pokemon-card">
-        <h4>{pokemon.name}</h4>
-        <img src={pokemon.sprites.other['official-artwork']['front_default']} />
-      </div>
+      <section className="pokemon-card">
+        <div className="pokemon-card-header">
+          <h3>{`Nº: ${pokemon.id}`}</h3>
+          <h3>{pokemon.name.capitalize()}</h3>
+        </div>
+        <div className="pokemon-card-other-infos">
+          <img src={pokemon.sprites.other['official-artwork']['front_default']} />
+        </div>
+      </section>
     );
   }
 }
