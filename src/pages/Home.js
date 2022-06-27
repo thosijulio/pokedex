dc2fe33346a592dedbdddebbfd704c18dd8e9b72
thosijulio <thosijulio@gmail.com>
2022-06-27 10:14:08 -0300

    style - correcao de posicionamento

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import './Home.css';
 
 function Home() {
-  return <section>Main Page</section>;
+  return (
+    <section>
+      <h3>
+        Welcome! This is a website to provide information about the Pokémon World developed by a
+        fan. if you have any questions, please get in touch.
+      </h3>
+      <p></p>
+    </section>
+  );
 }
 
 export default Home;
