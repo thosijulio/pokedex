8efec74aac023efbaf4137802f56c487b5fa67cc
thosijulio <thosijulio@gmail.com>
2022-07-04 10:47:48 -0300

    fix - correcao variavel isLoading (setar para true ao iniciar componente)

--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -8,12 +8,13 @@ import './PokemonDetails.css';
 function PokemonDetails() {
   const { pokemonId } = useParams();
   const [pokemon, setPokemon] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!Object.keys(pokemon).length) {
       const getPokemonInfos = async () => {
-        setIsLoading(true);
         const dataPokemon = await getPokemonByNameOrId(pokemonId);
         const dataPokemonSpecies = await getPokemonSpeciesByIdOrName(pokemonId);
         setPokemon({ ...dataPokemon, ...dataPokemonSpecies });
@@ -24,6 +25,31 @@ function PokemonDetails() {
     }
   }, [pokemon]);
 
+  useEffect(() => {
+    if (!images.length && !isLoading) {
+      const {
+        sprites,
+        sprites: {
+          other,
+          other: { dream_world, home }
+        }
+      } = pokemon;
+
+      setImages([
+        sprites.back_default,
+        sprites.back_shiny,
+        sprites.front_default,
+        sprites.front_shiny,
+        dream_world.front_default,
+        home.front_default,
+        home.front_shiny,
+        other['official-artwork'].front_default
+      ]);
+
+      setSelectedImage(other['official-artwork'].front_default);
+    }
+  }, [images, pokemon]);
+
   return !isLoading ? (
     <section>
       <div className="pokemon-details-header">
@@ -31,28 +57,28 @@ function PokemonDetails() {
         <h2>{pokemon.name}</h2>
       </div>
       <div className="pokemon-details-image-description">
-        {pokemon.sprites && (
-          <>
+        <img alt="pokemon" src={selectedImage} title={`${pokemon.name} artwork`} />
+        <p>
+          {
+            pokemon.flavor_text_entries.filter((entry) => entry.language.name === 'en')[0]
+              .flavor_text
+          }
+        </p>
+      </div>
+      <div className="pokemon-other-details">
+        <div id="pokemon-types">
+          {pokemon.types.map(({ type: { name } }, index) => (
             <img
               alt="pokemon"
-              src={pokemon.sprites.other['official-artwork']['front_default']}
-              title={`${pokemon.name} artwork`}
+              key={index}
+              src={require(`../images/pokemonTypes/${name}.png`)}
+              title={name}
             />
-            <p>{pokemon.flavor_text_entries[0].flavor_text}</p>
-          </>
-        )}
-      </div>
-      <div>
-        <div id="pokemon-types">
-          {pokemon.types &&
-            pokemon.types.map(({ type: { name } }, index) => (
-              <img
-                alt="pokemon"
-                key={index}
-                src={require(`../images/pokemonTypes/${name}.png`)}
-                title={name}
-              />
-            ))}
+          ))}
+        </div>
+        <div className="pokemon-shape">
+          <p>{`${pokemon.weight / 10} m`}</p>
+          <p>{`${pokemon.height / 10} kg`}</p>
         </div>
       </div>
     </section>
