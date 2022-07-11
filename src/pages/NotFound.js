567d31ac9282a095176d6e1769d32bea22a2be32
thosijulio <thosijulio@gmail.com>
2022-07-11 19:04:03 -0300

    style - estilizando pagina not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
 function NotFound() {
-  return <h1>Not Found Page</h1>;
+  return (
+    <main className="main-not-found">
+      <i className="fa-solid fa-ban fa-5x" />
+      <h1>Sorry, this page isn't available.</h1>
+      <h3>
+        The link you followed may not exists or are not implemented yet. Please, return to the{' '}
+        <Link to="/">main page.</Link>
+      </h3>
+    </main>
+  );
 }
 
 export default NotFound;
